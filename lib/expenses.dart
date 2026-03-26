36eda7657253e4e435292eb1a5fc86f2a524efa5
urubaalii <urubaali01@gmail.com>
2026-03-26 11:46:13 -0400

    Made dummy data in expenses

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/Expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,6 +11,20 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses =[
+  Expense(
+    title: "Gino's Pizza",
+    amount: 25.00,
+    date: DateTime.now(),
+    category: Category.food
+  ),
+  Expense(
+    title: "Train Ticket to MA",
+    amount: 15.25,
+    date: DateTime.now(),
+    category: Category.travel
+  ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/models/Expense.dart
+++ lib/models/Expense.dart
@@ -2,11 +2,15 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category {food, travel, leisure, work} //its own category variable
+
 class Expense{
-  Expense({required this.title, required this.amount, required this.date}) : id = uuid.v4();
+  Expense({required this.title, required this.amount, 
+  required this.date, required this.category}) : id = uuid.v4(); //random id generator
 
   final String id;  //this is all the data for an expense
   final String title;
   final double amount;
   final DateTime date;
+  final Category category; //string is too flexibile --> limit this to 4 cats using an enum --> leisure, travel, food, work --> then switched it to Category category
 }
