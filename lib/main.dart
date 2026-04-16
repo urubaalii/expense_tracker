283c8bfe658df784574aaf6f33580ad6f5ca0c74
urubaalii <urubaali01@gmail.com>
2026-04-16 11:33:01 -0400

    Finished dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart'; 
 
-var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+  );
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor:const Color.fromARGB(255, 5, 99, 125),);
+
+
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(
+    theme: ThemeData( //define theme of app: color, text, app bar, card, button
       textTheme: ThemeData().textTheme.copyWith(
         titleLarge: TextStyle(
           fontWeight: FontWeight.normal,
@@ -13,21 +21,27 @@ void main() {
         ),
       ),
 
-    colorScheme: kColorScheme,
-    appBarTheme: AppBarTheme(
-    backgroundColor: kColorScheme.onPrimaryContainer, 
-    foregroundColor: kColorScheme.primaryContainer),
-
-    cardTheme: CardThemeData(color: kColorScheme.secondaryContainer,
-    margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-    ), 
-
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: kColorScheme.primaryContainer),
     )
     ),
 
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kColorScheme,
+    cardTheme: CardThemeData(
+      color: kColorScheme.secondaryContainer,
+    margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    ), 
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: kColorScheme.primaryContainer,
+        foregroundColor: kDarkColorScheme.onPrimaryContainer
+    ),
+    )
+    ),
+
+themeMode: ThemeMode.system,
     home: Expenses(),
   )
   );
